27040e32df203940bb6659eecbf7b7a2beb9c37a
alfredo <yoelvisjimenezr@gmail.com>
2021-08-09 20:12:25 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import * as tq from 'type-graphql';
+import { ApolloServer } from 'apollo-server';
+import { PrismaClient } from '@prisma/client';
+import { resolvers } from '@generated/type-graphql';
+
+const prisma = new PrismaClient();
+
+const app = async () => {
+    const schema = await tq.buildSchema({ resolvers });
+
+    const context = () => {
+        return {
+            prisma
+        };
+    };
+
+    new ApolloServer({ schema, context }).listen({ port: 4000 }, () => {
+        console.log('🚀 Server ready at: http://localhost:4000');
+    });
+};
+
+app();
